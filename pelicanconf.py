91c1d84df463addf31ddac8cc5a6196e154d23eb
Joao Loula <joao.campos-loula@polytechnique.edu>
2016-04-01 23:57:01 +0200

    customization

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,13 +19,17 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
 # Social widget
+SOCIAL = (
+    ('github-alt', 'https://github.com/joaoloula'),
+    ('linkedin', 'https://fr.linkedin.com/in/joão-loula-2836b9107'),
+	('envelope', 'mailto:joao.campos-loula@polytechnique.edu')
+)
+DISQUS_SITENAME = 'mydisqus'
+DISQUS_ON_PAGESE = True
+GOOGLE_ANALYTICS = 'UA-00000000-0'
+COVER_IMG_URL = '/home/loula/Pictures/Wallpapers/reading_mountains.jpg'
+PROFILE_IMG_URL = '/home/loula/Pictures/Joao/profilepic_june.jpg'
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
